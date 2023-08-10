af8a72a3b55fadee580c12b1f4d73f52d4d001b7
wldnd9904 <wldnd9904@uos.ac.kr>
2023-08-10 21:56:44 +0900

    Initial Commit

---
+++ cokcok/cokcokApp.swift
@@ -0,0 +1,17 @@
+//
+//  cokcokApp.swift
+//  cokcok
+//
+//  Created by 최지웅 on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct cokcokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
